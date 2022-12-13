43914f3640c48bdeebdd2bebbdcaeb6069c0603a
Niklas Rodenbüsch <98448863+NI-R0@users.noreply.github.com>
2022-12-13 11:23:38 +0100

    Fixed last errors
    Signed-off-by: Niklas Rodenbüsch <98448863+NI-R0@users.noreply.github.com>

--- impressum.js
+++ impressum.js
@@ -2,6 +2,6 @@ const retBtn = document.getElementById("return_btn")
 
 retBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    window.open("/index.html", "_self");
+    window.open("./index.html", "_self");
     // window.scrollTo(0, 0);
 })
